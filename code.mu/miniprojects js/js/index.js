0fbc372d34673a4dad429324e97a473c4077a435
Sergey Rozhkov <borzsonite@gmail.com>
2020-03-12 17:14:03 +0300

    code.mu task-2 added

--- code.mu/miniprojects js/js/index.js
+++ code.mu/miniprojects js/js/index.js
@@ -3,7 +3,12 @@
 // Задача 1
 // Дан textarea.Пусть в него вводится текст.Сделайте так, чтобы по потери фокуса под текстареа вывелось сообщение о том, сколько в этом тексте слов.
 let textArea = document.getElementById('task-1');
-textArea.onblur = wordsCount;
+// textArea.onblur = wordsCount;
+// textArea.onblur = simbolsCount;
+// textArea.onblur = justSimbolsCount;
+textArea.onblur = SimbolsPercentCount;
+
+
 function wordsCount() {
     let arrOut = [];
     let arrIn = textArea.value.split(' ');
@@ -12,3 +17,45 @@ function wordsCount() {
     }
     alert(arrOut.length + 'слов');
 }
+
+function simbolsCount() {
+    alert(textArea.value.length + ' символов');
+}
+
+function justSimbolsCount() {
+    let arrOut = [];
+    let arrIn = textArea.value.split(' ');
+    for (let i = 0; i < arrIn.length; i++) {
+        if (arrIn[i].trim() != '') arrOut.push(arrIn[i])
+    }
+    alert(arrOut.join('').length + ' символов без пробелов');
+}
+let obj = {};
+
+function SimbolsPercentCount() {
+    let arrOut = [];
+    let arrIn = textArea.value.split(' ');
+    for (let i = 0; i < arrIn.length; i++) {
+        if (arrIn[i].trim() != '') arrOut.push(arrIn[i])
+    }
+
+    let finalString = arrIn.join('');
+    let mySet = new Set(finalString);
+    let count;
+
+
+    for (let key of mySet) {
+        count = 0;
+        for (let j = 0; j < finalString.length; j++) {
+            if (finalString[j] == key) {
+                count++;
+            }
+        }
+        obj[key] = count;
+    }
+}
+let out;
+for (let key in obj) {
+    out += `${key}: ${obj[key]}% <br>`
+}
+alert(out);
